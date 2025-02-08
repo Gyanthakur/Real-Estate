40c599a2f57d49adfaefe688b3913c9ce675fe6f
Gyanthakur <gps.96169@gmail.com>
2025-02-08 14:46:09 +0530

    real estate

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ import userRoute from "./routes/user.route.js";
 const app = express();
 
 // app.use(cors({ origin:"http://localhost:5173", credentials: true }));
-app.use(cors({ origin:"https://real-estate-psi-six.vercel.app", credentials: true }));
+// app.use(cors({ origin:"https://real-estate-psi-six.vercel.app", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
