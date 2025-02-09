6a0912ad4721996e98de9ec9d3372746f0b99276
Gyanthakur <gps.96169@gmail.com>
2025-02-10 02:14:59 +0530

    real estate

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // baseURL: "http://localhost:8000/api", 
-  baseURL: "https://real-estate-12wq.onrender.com", 
+  baseURL: "https://real-estate-12wq.onrender.com/api", 
   withCredentials: true,
 });
 
